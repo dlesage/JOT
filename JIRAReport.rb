e8da7fff306a76c9cdabc1e0bb1cc58beca38815
Darrin Mison <dmison@me.com>
2011-03-12 22:20:11 +1000

    commandline processing now handled by optparse

--- JIRAReport.rb
+++ JIRAReport.rb
@@ -2,11 +2,11 @@
 require 'rubygems'
 require 'erb'
 require 'soap/driver'
+require 'optparse'
 require 'JIRA-SOAP-STUBS/defaultDriver.rb'
 require 'extension_methods.rb'
 
-
-
+# ======================================== 
 def getFilterID(name, token, driver)
   favFilters = driver.getFavouriteFilters(token)
   favFilters.each do |filter|
@@ -16,6 +16,7 @@ def getFilterID(name, token, driver)
   end
 end
 
+# ======================================== 
 def getIssuesFromFilter( username, password, filter)
   soapdriver = JIRA::JiraSoapService.new
   loginToken = soapdriver.login(username, password)
@@ -24,6 +25,7 @@ def getIssuesFromFilter( username, password, filter)
   return issues
 end
 
+# ======================================== 
 def getTemplateOutputForIssues(template_file, issues)
   template = ERB.new File.new(template_file).read, nil, "%"
   template.result(binding)
@@ -31,10 +33,40 @@ def getTemplateOutputForIssues(template_file, issues)
 end
 
 # ======================================== read commandline parameters
-template_file = ARGV[0]
-username = ARGV[1]
-password = ARGV[2]
-filter = ARGV[3]
+def processCommandLine
+  options = {}
 
-puts getTemplateOutputForIssues(template_file, getIssuesFromFilter(username, password, filter) )
+  optparse = OptionParser.new do |opts|
+    opts.banner = "JIRAReport.rb [options]"
+
+    options[:template_file] = ""
+    opts.on( '-t FILE', '--template FILE', 'template to use' ) do|file|
+      options[:template_file] = file
+    end
+  
+    options[:username] = ""
+    opts.on( '-u USERNAME', '--username USERNAME', 'JIRA username' ) do | username |
+      options[:username] = username
+    end
+   
+     options[:password] = ""
+     opts.on( '-p PASSWORD', '--password PASSWORD', 'JIRA password' ) do | password |
+       options[:password] = password
+     end
+      
+     options[:filter] = ""
+     opts.on( '-f FILTER', '--filter FILTER', 'JIRA filter' ) do | filter |
+       options[:filter] = filter
+     end
+  end
+  optparse.parse!
+  return options
+end
+
+# ======================================== 
+options = processCommandLine
+
+puts getTemplateOutputForIssues(options[:template_file], 
+  getIssuesFromFilter(options[:username], options[:password], options[:filter]) 
+)
 
