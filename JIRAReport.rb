10556d3f95b51cae32d0eca4aa7b55ff2f9160db
Darrin Mison <dmison@me.com>
2011-03-12 16:20:28 +1000

    refactored down into methods

--- JIRAReport.rb
+++ JIRAReport.rb
@@ -6,11 +6,6 @@ require 'JIRA-SOAP-STUBS/defaultDriver.rb'
 require 'extension_methods.rb'
 
 
-# ======================================== read commandline parameters
-template_file = ARGV[0]
-username = ARGV[1]
-password = ARGV[2]
-filter = ARGV[3]
 
 def getFilterID(name, token, driver)
   favFilters = driver.getFavouriteFilters(token)
@@ -21,14 +16,25 @@ def getFilterID(name, token, driver)
   end
 end
 
-soapdriver = JIRA::JiraSoapService.new
+def getIssuesFromFilter( username, password, filter)
+  soapdriver = JIRA::JiraSoapService.new
+  loginToken = soapdriver.login(username, password)
+  filterID = getFilterID(filter, loginToken, soapdriver)
+  issues = soapdriver.getIssuesFromFilter(loginToken, filterID)
+  return issues
+end
 
-loginToken = soapdriver.login(username, password)
-filterID = getFilterID(filter, loginToken, soapdriver)
-issues = soapdriver.getIssuesFromFilter(loginToken, filterID)
+def getTemplateOutputForIssues(template_file, issues)
+  template = ERB.new File.new(template_file).read, nil, "%"
+  template.result(binding)
+  return template.result(binding)
+end
 
-#    puts jira.call_driver("getSecurityLevel",issue.key).inspect
+# ======================================== read commandline parameters
+template_file = ARGV[0]
+username = ARGV[1]
+password = ARGV[2]
+filter = ARGV[3]
 
-template = ERB.new File.new(template_file).read, nil, "%"
-template.result(binding)
-puts template.result(binding)
+puts getTemplateOutputForIssues(template_file, getIssuesFromFilter(username, password, filter) )
+
